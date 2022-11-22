a173740ded88089a880ced01849299dba661276d
rebuilt <memoryman51@hotmail.com>
2022-11-21 16:44:27 -0800

    add research page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :sqm, only: :index
   resources :qpa, only: :index
   resources :resource, only: :index
+  resources :research, only: :index
   resources :work_with_ecp, only: :index
   resources :construction, only: :index
   resources :district_leader, only: :index
