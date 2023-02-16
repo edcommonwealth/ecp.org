760d038670554f42dd8a074a62d9c311c2b40225
rebuilt <memoryman51@hotmail.com>
2023-02-16 08:45:13 -0800

    Add before action to set analytics id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :set_google_analytics_id
+
   def set_google_analytics_id
     @google_analytics_id = ENV['GOOGLE_ANALYTICS_ID']
   end
